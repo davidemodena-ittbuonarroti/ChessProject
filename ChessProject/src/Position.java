931ed448c8ac0f9587d37e7bc5769d7cae4f7f53
davidemodena-ittbuonarroti <davide.modena@buonarroti.tn.it>
2023-02-05 20:43:13 +0100

    feat: pasted initial code of Position class from Replit

--- ChessProject/src/Position.java
+++ ChessProject/src/Position.java
@@ -1,2 +1,32 @@
-public class Position {
+public class Position{
+    private int row;
+    private char col;
+
+    public Position(int row, int col){
+        //complete
+    }
+
+    public int getRow(){
+        //complete
+    }
+
+    public int getCol(){
+        //complete
+    }
+
+    public boolean setPosition(int row,int col){
+        //complete
+    }
+
+    public boolean setPosition(Position pos){
+        //complete
+    }
+
+    public boolean isValid(){
+        //complete
+    }
+
+    public boolean equals(Position pos){
+
+    }
 }
