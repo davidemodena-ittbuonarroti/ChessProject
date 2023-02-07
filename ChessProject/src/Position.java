0f41f77a9310f433d9de0a33e10111c00dcc6824
Stefano.Plotegher <stefano.plotegher@buonarroti.tn.it>
2023-02-07 18:44:59 +0100

    feat: aggiunto il progetto ChessProjegt

---
+++ ChessProject/src/Position.java
@@ -0,0 +1,38 @@
+public class Position{
+    private int row;
+    private char col;
+
+    public Position(int row, char col){
+        this.row = row;
+        this.col = col;
+    }
+
+    public int getRow(){
+        return row;
+    }
+
+    public char getCol(){
+        return col;
+    }
+
+    public boolean setPosition(int row,int col){
+        //complete
+    }
+
+    public boolean setPosition(Position pos){
+        //complete
+    }
+
+    public boolean isValid(){
+        //complete
+    }
+
+    public boolean equals(Position pos){
+        if(pos.getRow() == row && pos.getCol() == col){
+            return true;
+        }
+        else{
+            return false;
+        }
+    }
+}
