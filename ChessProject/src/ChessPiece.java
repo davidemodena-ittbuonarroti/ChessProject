c9e0dc26bc887439fb14f6b05a5e00631c7958b7
Stefano.Plotegher <stefano.plotegher@buonarroti.tn.it>
2023-02-07 18:57:12 +0100

    feat: aggiunto metodo per muovere la pedina

--- ChessProject/src/ChessPiece.java
+++ ChessProject/src/ChessPiece.java
@@ -1,4 +1,4 @@
-public class ChessPiece {
+public abstract class ChessPiece {
     private Position pos;
 
     public ChessPiece(Position pos){
@@ -7,5 +7,14 @@ public class ChessPiece {
 
     public Position getPosition(){  //ritorna la posizione del pezzo
         return this.pos;
+    }
+
+    public boolean moveTo(Position pos){ //metodo per muovere la pedina
+        boolean moved=false;
+        if(this.validMoves().contains(pos)){
+            this.pos = pos;
+            moved = true;
+        }
+        return moved;
     }
 }
