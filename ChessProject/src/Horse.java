988e80be3c88629fea8da5433de682b0f298e1b2
guido <guido.rodler@buonarroti.tn.it>
2023-02-08 21:59:00 +0100

    feat:added default method to class Horse

--- ChessProject/src/Horse.java
+++ ChessProject/src/Horse.java
@@ -1,2 +1,25 @@
-public class Horse {
+import java.util.ArrayList;
+
+public class Horse extends ChessPiece{
+    Horse(Position pos){
+        super(pos);
+    }
+
+    @Override
+    public ArrayList<Position> validMoves() {
+        ArrayList<Position> P= new ArrayList<Position>();
+        return P;
+    }
+
+    @Override
+    public boolean moveTo(Position pos) {
+        return super.moveTo(pos);
+
+    }
+
+    @Override
+    public Position getPosition() {
+        return super.getPosition();
+    }
 }
+
