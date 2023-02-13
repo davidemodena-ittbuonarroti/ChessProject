98716db23e9e619d4fb23839d1cf0caf4b1ea2fd
Mattia <mattia.conci@buonarroti.tn.it>
2023-02-13 15:40:05 +0100

    added and implemented method validMoves

--- ChessProject/src/Pawn.java
+++ ChessProject/src/Pawn.java
@@ -1,9 +1,30 @@
+import java.util.ArrayList;
+
 public class Pawn extends ChessPiece{
     private boolean firstMove;
 
     public Pawn(Position pos){
         super(pos);
         this.firstMove = true;
+    }
+
+    @Override
+    public ArrayList<Position> validMoves() {
+        ArrayList<Position> moves = new ArrayList<Position>();
+        int currentRow = this.getPosition().getRow();
+        char currentCol = this.getPosition().getCol();
+
+        int forward = currentRow + 1;
+        if(Position.isValid(forward, currentCol)){
+            moves.add(new Position(forward, currentCol));
+        }
+        if(firstMove){
+            forward = currentRow + 2;
+            if(Position.isValid(forward, currentCol)){
+                moves.add(new Position(forward, currentCol));
+            }
+        }
+        return moves;
     }
 
     public boolean moveTo(Position pos){
