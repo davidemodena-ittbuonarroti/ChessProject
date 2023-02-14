32ffc7e0265a3e9fbd0f8ac97a74c813a5bfbeed
davidemodena-ittbuonarroti <davide.modena@buonarroti.tn.it>
2023-02-14 21:07:41 +0100

    feat: created menu in main

--- ChessProject/src/Main.java
+++ ChessProject/src/Main.java
@@ -1,5 +1,34 @@
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        int scelta = 0;
+        do{
+            System.out.println("0) Exit");
+            System.out.println("1) Tower");
+            System.out.println("2) Pawn");
+            System.out.println("3) Bishop");
+            System.out.println("4) Horse");
+            System.out.print("Opzione: ");
+
+            switch (scelta){
+                case 0:
+
+                    break;
+                case 0:
+
+                    break;
+                case 0:
+
+                    break;
+                case 0:
+
+                    break;
+                case 0:
+
+                    break;
+                default:
+
+                    break;
+            }
+        }while(scelta!=0);
     }
 }
