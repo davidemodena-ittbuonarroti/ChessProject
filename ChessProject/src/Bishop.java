c3429b4fc2e6e576636860b74b71642d91e454ba
davidemodena-ittbuonarroti <davide.modena@buonarroti.tn.it>
2023-02-14 20:34:21 +0100

    feat: added method addDiagonalPositions

--- ChessProject/src/Bishop.java
+++ ChessProject/src/Bishop.java
@@ -5,4 +5,13 @@ public class Bishop extends ChessPiece{
     Bishop(Position pos){
         super(pos);
     }
+
+    private void addDiagonalPositions(int row, int diff, List<Position> positions) {
+        Position rightTarget = getPosition().add(row, diff);   //Add towards right
+        if(rightTarget != null) positions.add(rightTarget);
+
+        Position leftTarget = getPosition().add(row, -diff);   //Subtract (go towards left)
+        if(leftTarget != null) positions.add(leftTarget);
+    }
+}
 }
