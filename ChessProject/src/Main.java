f4df4ddb861304c30cdbc0d0fd394aa50cd5cf33
davidemodena-ittbuonarroti <davide.modena@buonarroti.tn.it>
2023-02-14 21:14:03 +0100

    feat: added case 1

--- ChessProject/src/Main.java
+++ ChessProject/src/Main.java
@@ -1,20 +1,30 @@
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
         int scelta = 0;
+        int row;
+        char col;
         do{
             System.out.println("0) Exit");
             System.out.println("1) Tower");
             System.out.println("2) Pawn");
             System.out.println("3) Bishop");
             System.out.println("4) Horse");
-            System.out.print("Opzione: ");
+            System.out.print("Option: ");
 
             switch (scelta){
                 case 0:
-                    System.out.println("Grazie e arrivederci!");
+                    System.out.println("Bye bye!");
                     break;
                 case 1:
-
+                    System.out.print("Inserisci una riga: ");
+                    row = input.nextInt();
+                    System.out.println("Inserisci una colonna: ");
+                    col = input.next().charAt(0);
+                    tower t = new tower(new Position(row,col));
+                    System.out.println("Le mosse disponibili sono: "+t.validMoves());
                     break;
                 case 2:
 
