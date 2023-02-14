cb6295e0aea255ca619610cfa0a8eea23713da4a
guido <guido.rodler@buonarroti.tn.it>
2023-02-14 20:41:11 +0100

    feat:added method validMoves

--- ChessProject/src/Horse.java
+++ ChessProject/src/Horse.java
@@ -8,6 +8,25 @@ public class Horse extends ChessPiece{
     @Override
     public ArrayList<Position> validMoves() {
         ArrayList<Position> P= new ArrayList<Position>();
+        int number =super.getPosition().getRow();
+        char letter=super.getPosition().getCol();
+        Position valid = new Position(0,'a');
+        int i=0;
+        while (i<=8){
+            if (i!=number){// check the rows
+                valid.setPosition(i, letter);
+                P.add(valid);
+            }
+            i++;
+        }
+        char j='a';
+        while (j <= 'h'){
+            if (j!=letter){// check the rows
+                valid.setPosition(number, j);
+                P.add(valid);
+            }
+            j++;
+        }
         return P;
     }
 
